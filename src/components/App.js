f441a33df663104cec52abda80bacbfc2b47c411
joelcrawford <joelrootcrawford@gmail.com>
2022-02-28 15:47:30 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+
+import Map from './Map/Map'
+
+export default function App() {
+    return (
+        <div>
+            <Map />
+        </div>
+    )
+}
---
+++ src/components/Map/Map.js
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react'
+import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+import './Map.css'
+import Tooltip from '../Tooltip/Tooltip'
+import ReactDOM from 'react-dom'
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY
+console.log(process.env)
+const Map = () => {
+    const mapContainerRef = useRef(null)
+    const tooltipRef = useRef(new mapboxgl.Popup({ offset: 15 }))
+
+    // Initialize map when component mounts
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: process.env.REACT_APP_MAPBOX_STYLE,
+            center: [94.628764, 37.554383],
+            zoom: 4
+        })
+
+        // change cursor to pointer when user hovers over a clickable feature
+        map.on('mouseenter', (e) => {
+            if (e.features.length) {
+                map.getCanvas().style.cursor = 'pointer'
+            }
+        })
+
+        // reset cursor to default when user is no longer hovering over a clickable feature
+        map.on('mouseleave', () => {
+            map.getCanvas().style.cursor = ''
+        })
+
+        // add tooltip when users mouse move over a point
+        map.on('mousemove', (e) => {
+            const features = map.queryRenderedFeatures(e.point)
+            if (features.length) {
+                const feature = features[0]
+
+                // Create tooltip node
+                const tooltipNode = document.createElement('div')
+                ReactDOM.render(<Tooltip feature={feature} />, tooltipNode)
+
+                // Set tooltip on map
+                tooltipRef.current
+                    .setLngLat(e.lngLat)
+                    .setDOMContent(tooltipNode)
+                    .addTo(map)
+            }
+        })
+
+        // Clean up on unmount
+        return () => map.remove()
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+            <div className='map-container' ref={mapContainerRef} />
+        </div>
+    )
+}
+
+export default Map
